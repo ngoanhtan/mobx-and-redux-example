4432268da690d78a5fe9cb83fda94af722ec215a
ngo.van.anh.tan <ngo.van.anh.tan@sun-asterisk.com>
2019-10-19 11:51:05 +0700

    Create Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './assets/App.css';
+import './assets/Style.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table className="root">
+        <tbody>
+          <tr>
+            <td className="left">
+              <div className="left-content-wrapper">
+                <div className="left-content">
+                  <div className="App">
+                    <header className="App-header">
+                      <img src={logo} className="App-logo" alt="logo" />
+                      <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                      </p>
+                      <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Learn React
+                      </a>
+                    </header>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td className="right">
+              <div className="right-content">
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+
   );
 }
 
